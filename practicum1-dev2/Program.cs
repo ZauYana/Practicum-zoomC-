390e70ffbf4004208589dca8a54c0af5612d01fe
Zaulskaya Yana <zaulsakayana@gmail.com>
2021-11-06 12:18:49 +0300

    добавлены еще задачи

--- practicum1-dev2/Program.cs
+++ practicum1-dev2/Program.cs
@@ -7,20 +7,61 @@
  - элементы, больше 8
  - знакочередования*/
 
-int minValue = 2;
-int maxValue = 89;
-int [] array = new int [15];
-int index=0;
-while (index<array.Length)
+
+//Create array
+int minValue = -20;
+int maxValue = 18;
+int[] array = new int[15];
+int index = 0;
+while (index < array.Length)
 {
-    array[index] = new Random ().Next(minValue,maxValue);
+    array[index] = new Random().Next(minValue, maxValue);
     index++;
 
 }
 
 index = 0;
-while (index<array.Length)
+while (index < array.Length)
 {
-    Console.WriteLine (array[index]);
+    Console.WriteLine(array[index]);
     index++;
 }
+Console.WriteLine("Создали массив");
+Console.WriteLine();
+
+
+//Create array 2 with growing up numbers
+
+int FE = array[0];
+Console.WriteLine(FE);
+index = 1;
+while (index < 15)
+{
+    if (array[index] > FE)
+    {
+        Console.WriteLine(array[index]);
+        FE = array[index];
+    }
+
+    index++;
+}
+
+Console.WriteLine("Создали массив по возрастанию из массива Array");
+Console.WriteLine();
+
+//only elements bigger than 8
+
+index = 0;
+while (index<15)
+{
+    if (array[index]>8)
+    {
+        Console.WriteLine (array[index]);
+    }
+    index++;
+
+}
+
+Console.WriteLine("Создали выборку всех элементов больше числа 8");
+Console.WriteLine();
+
